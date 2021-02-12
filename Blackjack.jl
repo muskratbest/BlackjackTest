140f752c947d25e83f235ae049d5db6d6ce09f20
muskratbest <78137267+muskratbest@users.noreply.github.com>
2021-02-12 13:26:45 -0500

    Update Blackjack.jl
    Fixed bug where the max number of decks used was 2

--- Blackjack.jl
+++ Blackjack.jl
@@ -226,11 +226,13 @@ function Shuffle(numofdecks)
         King = 10
 
         Deck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, Jack, Jack, Jack, Jack, Queen, Queen, Queen, Queen, King, King, King, King, 11, 11, 11, 11]
-        Deck2 = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11]
+        Deck2 = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, Jack, Jack, Jack, Jack, Queen, Queen, Queen, Queen, King, King, King, King, 11, 11, 11, 11]
 
         if numofdecks > 1
-                for i = numofdecks - 1
+                appendflag = numofdecks - 1
+                while appendflag > 0
                         append!(Deck,Deck2)
+                        appendflag -= 1
                 end
         end
 
